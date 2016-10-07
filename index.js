22c3f0b8db83a2dc65e5766052b8aeca20a0cd85
Andy Dayton <amdayton@gmail.com>
2016-10-07 17:07:02 -0400

    Basic functionality is there

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ var columnTpl = require('./column.hbs');
 // http://stackoverflow.com/questions/2924563/access-google-apps-public-spreadsheet-via-javascript
 var url = "https://spreadsheets.google.com/feeds/cells/134DTw4sCgMuNVZEYUdvhYN_09uEQq4kfNxWUgEeB2Eg/od6/public/values?alt=json-in-script&callback=?";
 
-
 $.getJSON(url).then(dataLoaded);
 
 
@@ -25,30 +24,84 @@ function dataLoaded(data) {
 	render(columns);
 }
 
+
 function render(columns) {
 	console.log(columns);
 
+	var header1 = columns[0].shift();
+	var header2 = columns[1].shift();
+
 	var colData1 = {
-		header: columns[0].shift(),
 		columns: columns[0]
 	}
 
 	var colData2 = {
-		header: columns[1].shift(),
 		columns: columns[1]
 	}
 
-	var columnEl1 = document.createElement('div');
-	columnEl1.id = 'col1';
-	columnEl1.className = 'column';
-	columnEl1.innerHTML = columnTpl(colData1);
+	var $headers = $('<header>');
+	$headers
+		.attr('id', 'headers')
+		.appendTo('body');
 
-	var columnEl2 = document.createElement('div');
-	columnEl2.id = 'col2';
-	columnEl2.className = 'column';
-	columnEl2.innerHTML = columnTpl(colData2);
+	$header1 = $('<div>')
+		.addClass('cell')
+		.addClass('header')
+		.html(header1)
+		.appendTo($headers);
 
-	document.body.appendChild(columnEl1);
-	document.body.appendChild(columnEl2);
+	$header2 = $('<div>')
+		.addClass('cell')
+		.addClass('header')
+		.html(header2)
+		.appendTo($headers);
+
+	var $column1 = $('<div>');
+	$column1
+		.attr('id', 'col1')
+		.addClass('column')
+		.html(columnTpl(colData1))
+		.appendTo('body');
+
+	var $column2 = $('<div>');
+	$column2
+		.attr('id', 'col2')
+		.addClass('column')
+		.html(columnTpl(colData2))
+		.appendTo('body');
+
+	$column1.scrollTop(($column1.find('.shim').height()/2)-($column1.height()/2));
+	$column2.scrollTop(($column2.find('.shim').height()/2)-($column2.height()/2));
+
+	var col1Scrolling = false;
+	var col2Scrolling = false;
+	var interval = 100;
+	var intervalId;
+
+	$column1.on('scroll', function(e) {
+		var percent = $column1.height()/($column1.scrollTop() + ($column1.height()/2));
+		var translation = $column2.find('.shim').height()*percent - ($column2.height());
+		
+		if(!col2Scrolling && percent !== Infinity) {
+			$column2.scrollTop(translation);
+			col1Scrolling = true;
+			window.clearInterval(intervalId);
+			intervalId = window.setInterval(function() { col1Scrolling = false }, interval);
+		}
+	});
+
+
+	$column2.on('scroll', function(e) {
+		var percent = $column2.height()/($column2.scrollTop() + ($column2.height()/2));
+		var translation = $column1.find('.shim').height()*percent - ($column1.height());
+		
+		if(!col1Scrolling && translation !== Infinity) {
+			$column1.scrollTop(translation);
+			col2Scrolling = true;
+			window.clearInterval(intervalId);
+			intervalId = window.setInterval(function() { col2Scrolling = false }, interval);
+		}
+	});
+
 
 }
